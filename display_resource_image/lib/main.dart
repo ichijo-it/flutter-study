85e5e9622498e28e87a7c8176c1e42a25d98b32e
難波江 瞭有 R.N. <nabae.ryo@ithink.co.jp>
2026-05-13 17:46:30 +0900

    modified to display online image.

--- display_resource_image/lib/main.dart
+++ display_resource_image/lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const DisplayResourceImageApp());
+void main() => runApp(const DisplayImageApp());
 
-class DisplayResourceImageApp extends StatelessWidget {
-  const DisplayResourceImageApp({super.key});
+class DisplayImageApp extends StatelessWidget {
+  const DisplayImageApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('Display Resource Image Sample')),
+        appBar: AppBar(title: const Text('Display Image App Home')),
         body: const AppHomePage(),
       ),
     );
@@ -34,10 +34,10 @@ class AppHomePage extends StatelessWidget {
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => const DisplayResourceImagePage()),
+                MaterialPageRoute(builder: (context) => const DisplayImagePage()),
               );
             },
-            child: const Text('Display Resource Image'),
+            child: const Text('Display Image'),
           ),
         ],
       ),
@@ -45,30 +45,97 @@ class AppHomePage extends StatelessWidget {
   }
 }
 
-class DisplayResourceImagePage extends StatelessWidget {
-  const DisplayResourceImagePage({super.key});
+// 画像表示切り替えを行うため、状態を持つStatefulWidgetを作成
+class DisplayImagePage extends StatefulWidget {
+  const DisplayImagePage({super.key});
+
+  @override
+  State<DisplayImagePage> createState() => _DisplayImagePageState();
+}
+
+enum ImageDisplayMode {
+  asset,
+  network,
+  none,
+}
+
+class _DisplayImagePageState extends State<DisplayImagePage> {
+  ImageDisplayMode _displayMode = ImageDisplayMode.none;
+
+  void _updateDisplayMode(ImageDisplayMode mode) {
+    setState(() {
+      _displayMode = mode;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Display Resource Image')),
+      appBar: AppBar(title: const Text('Display Image')),
       body: Column(
         children: [
+          // 上ボタンエリア
           Expanded(
-            flex: 3,
+            flex: 1,
             child: Padding(
-              padding: const EdgeInsets.all(16.0),  // 全周に16の余白
-              child: Image.asset(
-                'assets/images/sample_image.png',
-                fit: BoxFit.contain,
+              // 上下左右8pxの余白
+              padding: const EdgeInsets.all(8.0), // px
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () =>
+                          _updateDisplayMode(ImageDisplayMode.asset),
+                      child: const Text('Asset Image'),
+                    ),
+                  ),
+
+                  // ボタン同士のスペースを空けるためにSizedBoxで空間を作成
+                  const SizedBox(width: 8), // px
+
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () =>
+                          _updateDisplayMode(ImageDisplayMode.network),
+                      child: const Text('Online Image'),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
+
+          // 画像エリア
+          Expanded(
+            flex: 6,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),  // px
+              child: SizedBox.expand(
+                child: _displayMode == ImageDisplayMode.asset
+                    ? Image.asset(
+                  'assets/images/sample_image.png',
+                  fit: BoxFit.contain,
+                )
+                    // Online上の画像は、以下のサービスを利用してプレースホルダー画像を表示する。
+                    //   https://placeholdpicsum.dev/。画像サイズやカテゴリを指定して簡単にプレースホルダー画像を生成できるサービス。
+                    //   > Generate custom placeholders via simple URLs. No signup required, free forever.
+                    : _displayMode == ImageDisplayMode.network
+                    ? Image.network(
+                  'https://placeholdpicsum.dev/photo/600/400',
+                  fit: BoxFit.contain,
+                    )
+                    : const SizedBox.shrink(),
+              ),
+            ),
+          ),
+
+          // 下ボタンエリア
           Expanded(
             flex: 1,
             child: Center(
               child: ElevatedButton(
                 onPressed: () {
+                  _updateDisplayMode(ImageDisplayMode.none);
                   Navigator.pop(context);
                 },
                 child: const Text('Go Back'),
